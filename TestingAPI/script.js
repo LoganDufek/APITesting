78f85c49865edb196ce9ff452cda9e995dbd5033
LoganDufek <logandufek@gmail.com>
2021-07-04 23:04:13 -0400

    code updated, search by movie functionality

--- TestingAPI/script.js
+++ TestingAPI/script.js
@@ -153,8 +153,7 @@ function checkSecondActor() {
                                 return response.json();
                             })
                             .then(function (response) {
-
-
+                                    
                                 var poster = response.Search[0].Poster
                                 var posterImg = document.createElement("img")
                                 posterImg.setAttribute("src", poster)
@@ -172,75 +171,26 @@ function checkSecondActor() {
             console.error(err);
         });
 
+} 
 
-    // console.log(filmArray1)
-    // console.log(filmArray2)
+function getCastList() {
 
+    userResonse3 = document.querySelector('.EnterFilm').value
 
-
+    fetch(
+        "http://www.omdbapi.com/?t=" + userResonse3 + "&apikey=d86e2804"
+    )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response)
+})
 }
 
 
-// // function checkActors2() {
-// //     userResponse1 = "Tom Hanks"
-// //     userResponse2 = "Chris Pratt"
 
-// //     Promise.all([
-// //         fetch("https://data-imdb1.p.rapidapi.com/actor/imdb_id_byName/" + userResponse1 + "/", {
-// //             "method": "GET",
-// //             "headers": {
-// //                 "x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
-// //                 "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
-// //             }
-// //         }),
-// //         fetch("https://data-imdb1.p.rapidapi.com/actor/imdb_id_byName/" + userResponse2 + "/", {
-// //             "method": "GET",
-// //             "headers": {
-// //                 "x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
-// //                 "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
-// //             }
-// //         })
-// //     ]).then(function (responses) {
-// //         // Get a JSON object from each of the responses
-// //         return Promise.all(responses.map(function (response) {
-// //             return response.json();
-// //         }));
-// //     }).then(function (data) {
-// //         // Log the data to the console
-// //         // You would do something with both sets of data here
-// //         console.log(data);
-
-// //         var ActorID = Promise.all(data.Array[0].Actors[0].imdb_id)
-// //         console.log(ActorID)
-
-
-// //         fetch("https://data-imdb1.p.rapidapi.com/actor/id/" + ActorID + "/all_roles/", {
-// //             "method": "GET",
-// //             "headers": {
-// //                 "x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
-// //                 "x-rapidapi-host": "data-imdb1.p.rapidapi.com"
-// //             }
-
-
-// //         })
-// //             .then(response => {
-// //                 return response.json();
-// //             })
-// //             .then(function (response) {
-// //                 for (i = 0; i < response.Roles.movies.length; i++) {
-// //                     filmArray = (response.Roles.movies[i][0].title)
-// //                     console.log(filmArray)
-// //                 }
-// //             })
-
-
-// //     }).catch(function (error) {
-// //         // if there's an error, log it
-// //         console.log(error);
-// //     });
-
-
-
-// // }
 
 document.getElementById("search").addEventListener("click", checkFirstActor)
+document.getElementById("FilmSearch").addEventListener("click", getCastList)
+
